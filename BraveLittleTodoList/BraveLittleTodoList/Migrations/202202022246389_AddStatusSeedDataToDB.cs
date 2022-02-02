63a5c6aae7eae6e86a0601ccb7011a356b1caea5
Carlos Chapa <bravelittletodolist@gmail.com>
2022-02-02 15:56:02 -0700

    Added StatusSeedDataToDB

---
+++ BraveLittleTodoList/BraveLittleTodoList/Migrations/202202022246389_AddStatusSeedDataToDB.cs
@@ -0,0 +1,20 @@
+﻿namespace BraveLittleTodoList.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStatusSeedDataToDB : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Status (Name) VALUES ('To Do')");
+            Sql("INSERT INTO Status (Name) VALUES ('In Progress')");
+            Sql("INSERT INTO Status (Name) VALUES ('Done')");
+        }
+        
+        public override void Down()
+        {
+            //You should add a way to roll back the migration
+        }
+    }
+}
