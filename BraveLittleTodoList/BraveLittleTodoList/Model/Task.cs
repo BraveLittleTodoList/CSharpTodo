fce7979af22e59c42bc3e9cc5d0f15572c5e4e5e
Carlos Chapa <bravelittletodolist@gmail.com>
2022-02-03 14:21:46 -0700

    Added tasks to model, added task to DbContext

---
+++ BraveLittleTodoList/BraveLittleTodoList/Model/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraveLittleTodoList.Model
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int StatusId { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
--- BraveLittleTodoList/BraveLittleTodoList/Model/tlDBContext.cs
+++ BraveLittleTodoList/BraveLittleTodoList/Model/tlDBContext.cs
@@ -6,5 +6,6 @@ namespace BraveLittleTodoList.Model
     class tlDBContext : DbContext
     {
         public DbSet<Status> Statuses { get; set; }
+        public DbSet<Task> Tasks { get; set; }
     }
 }
